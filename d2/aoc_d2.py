71a7decc5e99b2fbacd0f47d4a69ca39fad99933
gurk314 <gurkeerat.chhina@gmail.com>
2022-12-02 15:53:12 -0800

    AOC Day 2 - Part 2

--- d2/aoc_d2.py
+++ d2/aoc_d2.py
@@ -1,19 +1,30 @@
 input_file = 'd2/input.txt'
 
-def outcome(input):
-    return (input[1]-input[0]+1) % 3
+def score_given(input):
+    return 3*((input[1]-input[0]+1) % 3) + input[1] + 1
 
-def score(input):
-    return 3*outcome(input) + input[1] + 1
-
-def total_score(filename):
+def total_score_given(filename):
     sum = 0
     with open(filename) as file:
         for line in file:
             round = line.strip().split()
             round_int = [ord(round[0]) - ord("A"), ord(round[1]) - ord("X")]
-            sum += score(round_int)
+            sum += score_given(round_int)
     return sum
 
+def score_outcome(input):
+    return 3*input[1] + (input[0]+input[1]-1)%3 + 1
+
+def total_score_outcome(filename):
+    sum = 0
+    with open(filename) as file:
+        for line in file:
+            round = line.strip().split()
+            round_int = [ord(round[0]) - ord("A"), ord(round[1]) - ord("X")]
+            sum += score_outcome(round_int)
+    return sum
+        
+
 if __name__ == '__main__':
-    print(total_score(input_file))
+    print(total_score_given(input_file))
+    print(total_score_outcome(input_file))
