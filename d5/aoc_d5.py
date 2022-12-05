6485d42d717f340dba306de15dcdad4ffec422f7
gurk314 <gurkeerat.chhina@gmail.com>
2022-12-05 13:05:50 -0800

    AOC D5P1

---
+++ d5/aoc_d5.py
@@ -0,0 +1,33 @@
+input_file = 'd5/input.txt'
+
+def move_crate(amount, origin, destination):
+    destination[:] = list(reversed(origin[0:amount])) + destination
+    origin[:] = origin[amount:]
+
+def move_crates(filename, movement_function):
+    list_of_piles = []
+    with open(filename) as file:
+        line = file.readline()
+        num_piles = len(line)//4
+        list_of_piles = [[] for i in range(num_piles)]
+        while True:
+            try:
+                for i in range(num_piles):
+                    if line[1+4*i].strip():
+                        list_of_piles[i].append(line[1+4*i])
+                line = file.readline()
+            except:
+                break
+        while True:
+            try:
+                line = file.readline().split()
+                print(line)
+                move_crate(int(line[1]), list_of_piles[int(line[3])-1], list_of_piles[int(line[5])-1])
+            except:
+                break
+    print(list_of_piles)
+    return [pile[0] for pile in list_of_piles]
+
+
+if __name__ == '__main__':
+    print(*move_crates(input_file))
