6846d048752bd993b9f4a4f277f6445e248eb92b
gurk314 <gurkeerat.chhina@gmail.com>
2022-12-06 15:22:05 -0800

    d6p2

--- d6/aoc_d6.py
+++ d6/aoc_d6.py
@@ -3,12 +3,13 @@ input_file = 'd6/input.txt'
 def has_duplicates(list):
     return not len(list) == len(set(list))
 
-def find_marker(filename):
+def find_marker(filename, length):
     with open(filename) as file:
         line = file.readline()
         for i in range(len(line)):
-            if not has_duplicates(line[i:i+4]):
-                return i+4
+            if not has_duplicates(line[i:i+length]):
+                return i+length
 
 if __name__ == '__main__':
-    print(find_marker(input_file))
+    print(find_marker(input_file,4))
+    print(find_marker(input_file,14))
