549abd581a2af7d6ff6c4a5b2d584bad8d8760d8
gurk314 <gurkeerat.chhina@gmail.com>
2022-12-07 16:19:17 -0800

    d7 p2

--- d7/aoc_d7.py
+++ d7/aoc_d7.py
@@ -3,6 +3,9 @@ from ast import literal_eval
 
 input_file = 'd7/input.txt'
 
+total_space = 70000000
+needed_space = 30000000
+
 regex1 = r'(\$\scd\s[^\.\n]+\n)|(\$\scd\s\.\.\n)|(dir\s[a-z]+\n)|(\$\sls\n)|(\s[a-z\.]+\n)'
 regex2 = r'(,\s])|(,\s$)'
 
@@ -42,5 +45,6 @@ def regex_subs(string):
     return re.sub(regex2, replacements2, string)
 
 if __name__ == '__main__':
-    tree_sum(literal_eval(regex_subs(file_string(input_file))))
+    used_space = tree_sum(literal_eval(regex_subs(file_string(input_file))))
     print(sum([x for x in list_of_tree_sums if x <= 100000]))
+    print(min([x for x in list_of_tree_sums if x + (total_space-used_space)>needed_space]))
