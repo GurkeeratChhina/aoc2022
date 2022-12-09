d3e05e804672b5b0bcc5b4d376ffe25a2ce0d5a3
gurk314 <gurkeerat.chhina@gmail.com>
2022-12-08 16:44:47 -0800

    aoc d8 p2 quick and dirty, not the best solution

--- d8/aoc_d8.py
+++ d8/aoc_d8.py
@@ -7,7 +7,54 @@ def to_array(filename):
             array.append([int(digit) for digit in line.strip()])
     return array
 
-def visibility(tree_array):
+def visibility_up(tree_x, tree_y, tree_array):
+    sum = 0
+    for y in range(tree_y):
+        sum += 1
+        if tree_array[tree_y-y-1][tree_x] >= tree_array[tree_y][tree_x]:
+            break
+    return sum
+
+def visibility_down(tree_x, tree_y, tree_array):
+    sum = 0
+    for y in range(tree_y+1, len(tree_array)):
+        sum += 1
+        if tree_array[y][tree_x] >= tree_array[tree_y][tree_x]:
+            break
+    return sum
+
+def visibility_left(tree_x, tree_y, tree_array):
+    sum = 0
+    for x in range(tree_x):
+        sum += 1
+        if tree_array[tree_y][tree_x-x-1] >= tree_array[tree_y][tree_x]:
+            break
+    return sum
+
+def visibility_right(tree_x, tree_y, tree_array):
+    sum = 0
+    for x in range(tree_x+1, len(tree_array[0])):
+        sum += 1
+        if tree_array[tree_y][x] >= tree_array[tree_y][tree_x]:
+            break
+    return sum
+
+def tree_visibility(tree_x, tree_y, tree_array):
+    a = visibility_up(tree_x, tree_y, tree_array)
+    b = visibility_down(tree_x, tree_y, tree_array)
+    c = visibility_left(tree_x, tree_y, tree_array)
+    d = visibility_right(tree_x, tree_y, tree_array)
+    return a*b*c*d
+
+def find_max_tree(tree_array):
+    max = 0
+    for y in range(len(tree_array)):
+        for x in range(len(tree_array[0])):
+            if tree_visibility(x,y,tree_array) > max:
+                max = tree_visibility(x,y,tree_array)
+    return max
+
+def visibility_from_edge(tree_array):
     height = len(tree_array)
     width = len(tree_array[0])
     visibility_array = [[0 for x in range(width)] for y in range(height)]
@@ -55,5 +102,7 @@ def visibility(tree_array):
 
 if __name__ == '__main__':
     tree_array = to_array(input_file)
-    print(sum([sum(row) for row in (visibility(tree_array))]))
+    print(sum([sum(row) for row in (visibility_from_edge(tree_array))]))
+    print(find_max_tree(tree_array))
+    #print(find_max_tree(to_array('d8/test.txt')))
 
