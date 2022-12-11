3053a0ba9638435aa48f83bd1059e56ad35728cb
gurk314 <gurkeerat.chhina@gmail.com>
2022-12-11 11:23:47 -0800

    p2 with crt

--- d11/aoc_d11.py
+++ d11/aoc_d11.py
@@ -4,7 +4,7 @@ from sympy import lambdify
 input_file = 'd11/input.txt'
 
 class Monkey:
-    def __init__(self, list_of_items, if_true, if_false, throw_condition, worry_update, relief_function = lambda x: x//3):
+    def __init__(self, list_of_items, if_true, if_false, throw_condition, worry_update, relief_function):
         self.items = list_of_items
         self.next_true = if_true
         self.next_false = if_false
@@ -32,17 +32,18 @@ class MonkeyCollection:
             monkey.next_true = self.monkeys[monkey.next_true]
             monkey.next_false = self.monkeys[monkey.next_false]
     
-    def do_round(self):
-        for monkey in self.monkeys:
-            monkey.update_worries()
-            monkey.pass_items()
+    def do_round(self, amount):
+        for i in range(amount):
+            for monkey in self.monkeys:
+                monkey.update_worries()
+                monkey.pass_items()
 
     def monkey_business(self):
         inspections = sorted([monkey.total_inspected for monkey in self.monkeys])
         print(inspections)
         return inspections[-1]*inspections[-2]
         
-def parse_input(filename):
+def parse_input(filename, relief_function):
     list_of_monkeys = []
     with open(filename) as file:
         while(True):
@@ -54,13 +55,15 @@ def parse_input(filename):
                 pass_true = int(file.readline().strip().split(" ")[-1])
                 pass_false = int(file.readline().strip().split(" ")[-1])
                 file.readline()
-                list_of_monkeys.append(Monkey(items,pass_true,pass_false,test,worry_function))
+                list_of_monkeys.append(Monkey(items,pass_true,pass_false,test,worry_function, relief_function))
             except:
                 break
     return list_of_monkeys
 
 if __name__ == '__main__':
-    monkeys = MonkeyCollection(parse_input(input_file))
-    for i in range(20):
-        monkeys.do_round()
+    monkeys = MonkeyCollection(parse_input(input_file, lambda x: x//3))
+    monkeys.do_round(20)
     print(monkeys.monkey_business())
+    monkeys2 = MonkeyCollection(parse_input(input_file, lambda x: x%9699690))
+    monkeys2.do_round(10000)
+    print(monkeys2.monkey_business())
