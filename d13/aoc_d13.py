3b454262ba852138ac563144082743bd40d30eed
gurk314 <gurkeerat.chhina@gmail.com>
2022-12-12 23:47:14 -0800

    rename

--- d13/aoc_d13.py
+++ d13/aoc_d13.py
@@ -1,18 +1,18 @@
 input_file = 'd13/input.txt'
 
-def compare_lists(list1, list2):
+def is_less_than(list1, list2):
     if isinstance(list1, int) and isinstance(list2, int):
         if list1 == list2:
             return "equal"
         else:
             return list1 < list2
     elif isinstance(list1, int) and isinstance(list2, list):
-        return compare_lists([list1], list2)
+        return is_less_than([list1], list2)
     elif isinstance(list1, list) and isinstance(list2, int):
-        return compare_lists(list1, [list2])
+        return is_less_than(list1, [list2])
     else:
         for i in range(min(len(list1), len(list2))):
-            comparison = compare_lists(list1[i], list2[i])
+            comparison = is_less_than(list1[i], list2[i])
             if comparison != "equal":
                 return comparison
         if len(list1) == len(list2):
@@ -30,17 +30,17 @@ def parse(filename, first, second):
             try:
                 first_list = eval(file.readline().strip())
                 second_list = eval(file.readline().strip())
-                if compare_lists(first_list,second_list):
+                if is_less_than(first_list,second_list):
                     sum += pair_index
-                if compare_lists(first_list, first):
+                if is_less_than(first_list, first):
                     index_first += 1
                     index_second += 1
-                elif compare_lists(first_list, second):
+                elif is_less_than(first_list, second):
                     index_second += 1
-                if compare_lists(second_list, first):
+                if is_less_than(second_list, first):
                     index_first += 1
                     index_second += 1
-                elif compare_lists(second_list, second):
+                elif is_less_than(second_list, second):
                     index_second += 1
                 pair_index += 1
                 file.readline()
